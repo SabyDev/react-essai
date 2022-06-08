fc34a70da9200ba1208b56b8c6d83d2cdb1266ac
sabrina michee <martmichee@gmail.com>
2022-06-08 16:49:47 +0200

    photo

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import Contenu from '../src/Contenu/Contenu';
 import Emoji from './Emoji/Emoji';
 import Footer from '../src/Footer/Footer'
 import Header from '../src/Header/Header'
-import Produit from '../src/Produit/Produit'
-import Message from '../src/Message/Message'
+// import Produit from '../src/Produit/Produit'
+// import Message from '../src/Message/Message'
 import photo1 from '../src/img/siege1.webp'
+import photo2 from '../src/img/images.jpg'
+import photo3 from '../src/img/photo3.jpg'
 import Produits from '../src/Produit/Produit';
 // injecter du code dans l index html via #root
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -20,8 +22,8 @@ root.render(
     <Emoji/>
     <Contenu />
     <Produits img={photo1} titre= 'siège Gamer' description='siège confort pour gamer 24/24' price='2500€' avis='⭐⭐⭐⭐' />
-    <Produits img={photo1} titre= 'siège Gamer' description='siège confort pour gamer 24/24' price='2500€' avis='⭐⭐⭐⭐' />
-    <Produits img={photo1} titre= 'siège Gamer' description='siège confort pour gamer 24/24' price='2500€' avis='⭐⭐⭐⭐' />
+    <Produits img={photo2} titre= 'siège Gamer' description='siège confort ' price='200€' avis='⭐⭐' />
+    <Produits img={photo3} titre= 'siège Gamer' description='siège confort pour wc' price='1500€' avis='⭐⭐⭐⭐' />
     <Footer />
   </React.StrictMode>
 );
