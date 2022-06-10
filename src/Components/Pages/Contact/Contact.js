da2236eebc6da6c39641c092f2a639b41890f28d
sabrina michee <martmichee@gmail.com>
2022-06-10 12:44:58 +0200

    choix deroulant dans formulaire

--- src/Components/Pages/Contact/Contact.js
+++ src/Components/Pages/Contact/Contact.js
@@ -9,35 +9,47 @@ export default function Contact (){
         const[firstName,setFirstName]= useState("");
         const[PugName,setPugName]= useState("");
         const[Email,setEmail]= useState("");
-
-        const validation = (event) => {
-            event.preventDefault ();
-            alert (`'Votre prénom est' : ${firstName}`)
-       }
+        const[Taille, setTaille] = useState("s");       
+        
+       const validationTaille = (event) => {
+        event.preventDefault ();
+        alert (`'Vous avez selectionner la taille' : ${Taille}`)
+   }
     return (
         <div className="formulaire">
         <h1>Contact</h1>
-        <form className='champs' onSubmit={validation}>
+        <form className='champs' onSubmit={validationTaille}>
             <label >Nom : 
                 <input type="text"
                 value={name} 
-                onChange={(element) => setName(element.target.value)}/>
+                onChange={(element) => setName(element.target.value)} required/>
             </label>
             <label>Prénom :  
                 <input type="text"
                 value={firstName} 
-                onChange={(element) => setFirstName(element.target.value)}/>
+                onChange={(element) => setFirstName(element.target.value)} required/>
             </label>
             <label>Nom du Pug : 
                 <input type="text"
                 value={PugName} 
-                onChange={(element) => setPugName(element.target.value)}/>
+                onChange={(element) => setPugName(element.target.value)}required/>
             </label>
             <label>Email : 
                 <input type="text"
                 value={Email} 
-                onChange={(element) => setEmail(element.target.value)}/>
+                onChange={(element) => setEmail(element.target.value)}required/>
             </label>
+            <label>
+          Choisir la taille de votre Pug:
+          <select value={Taille}
+           onChange={(element) => setTaille(element.target.value)}required>
+            <option value="S">S</option>
+            <option value="M">M</option>
+            <option value="L">L</option>
+            <option value="XL">XL</option>
+          </select>
+        </label>
+        
             <input type="submit" />
         </form>
 
