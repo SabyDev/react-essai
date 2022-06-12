ce627db2be04d27e3e3b418dc7c21ec718209f03
sabrina michee <martmichee@gmail.com>
2022-06-12 11:18:25 +0200

    création d'un modal

--- src/Components/Components_pages/Header/Header.js
+++ src/Components/Components_pages/Header/Header.js
@@ -3,18 +3,32 @@ import'./Header.css'
 import crotte from '../../../style/assets/img/poop.png';
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+// importation du HOOK
+import { useState } from 'react';
+import Modal from '../Modal/Modal';
 
 // j'exporte une fonction header qui retourne mon header
 export default function Header(){
+  const [show, setShow] = useState(false);
+function handleModal(){  
+  setShow(!show);
+}
     return (
         <header className="App-header">
         <img src={crotte} className="logo-crotte" alt="logo de pug compagnie" />
+        <div className="modalContenu">
+          <button onClick={handleModal}>
+            {show ? 'cacher' : 'montrer'} la modal
+          </button>
+          {show && <Modal/>}
+        </div>
+
         <nav>
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="produit">Déguisements</Link></li>
             <li><Link to="contact">Contact</Link></li>
-            <li><Link to="post">Posts</Link></li>
+            <li><Link to="post">Blog</Link></li>
           </ul>
         </nav>
         <Outlet/>
---
+++ src/Components/Components_pages/Modal/Modal.js
@@ -0,0 +1,9 @@
+import '../Modal/Modal.css'
+
+export default function Modal(){
+    return(
+        <div className="modal">
+            <h1> Bienvenue <img src="https://media.istockphoto.com/vectors/cute-neon-pug-in-a-pink-tutu-dress-wearing-a-crown-vector-art-of-an-vector-id1314067725?b=1&k=20&m=1314067725&s=170667a&w=0&h=8UVtTnzvSzoqgta4bhQlyuJDh0ZjIFM6lYRVnkJgVO8=" alt="" /></h1>
+        </div>
+    )
+}
